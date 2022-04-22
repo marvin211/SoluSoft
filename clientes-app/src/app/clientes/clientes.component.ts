fbfb2cb292fd1e2bc0bae009fc78106ec61cd6f4
KeyzerH <keyzerhernandez.kjnhns@gmail.com>
2022-04-22 12:47:35 -0600

    Archivo clientes.json.ts con la lista de objetos creado

--- clientes-app/src/app/clientes/clientes.component.ts
+++ clientes-app/src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
@@ -7,151 +8,11 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] = [
-  {
-		id: 1,
-		nombre: 'Maisie',
-		apellido: 'Serrano',
-		email: 'aliquet.vel@yahoo.com',
-		createAt: '2022-05-05'
-	},
-	{
-		id: 2,
-		nombre: 'Dana',
-		apellido: 'Lozano',
-		email: 'quis.pede@aol.couk',
-		createAt: '2022-04-20'
-	},
-	{
-		id: 3,
-		nombre: 'Kimberley',
-		apellido: 'Delgado',
-		email: 'cubilia.curae.donec@outlook.net',
-		createAt: '2022-04-02'
-	},
-	{
-		id: 4,
-		nombre: 'Ralph',
-		apellido: 'Ramos',
-		email: 'hendrerit.neque@google.edu',
-		createAt: '2022-04-30'
-	},
-	{
-		id: 5,
-		nombre: 'Hayden',
-		apellido: 'Hernandez',
-		email: 'nulla.integer.urna@aol.net',
-		createAt: '2022-05-16'
-	},
-	{
-		id: 6,
-		nombre: 'Micah',
-		apellido: 'Cruz',
-		email: 'accumsan.interdum@yahoo.net',
-		createAt: '2022-04-18'
-	},
-	{
-		id: 7,
-		nombre: 'Neil',
-		apellido: 'Gomez',
-		email: 'eget.magna@icloud.couk',
-		createAt: '2022-05-11'
-	},
-	{
-		id: 8,
-		nombre: 'Ali',
-		apellido: 'Gutierrez',
-		email: 'a@hotmail.couk',
-		createAt: '2022-05-24'
-	},
-	{
-		id: 9,
-		nombre: 'Slade',
-		apellido: 'Mora',
-		email: 'dui.cum.sociis@hotmail.couk',
-		createAt: '2022-04-13'
-	},
-	{
-		id: 10,
-		nombre: 'Geraldine',
-		apellido: 'Gallego',
-		email: 'diam.sed.diam@protonmail.com',
-		createAt: '2022-04-20'
-	},
-	{
-		id: 11,
-		nombre: 'Elton',
-		apellido: 'Miguel',
-		email: 'ligula.elit@outlook.ca',
-		createAt: '2022-05-19'
-	},
-	{
-		id: 12,
-		nombre: 'Amery',
-		apellido: 'Campos',
-		email: 'orci@aol.couk',
-		createAt: '2022-04-07'
-	},
-	{
-		id: 13,
-		nombre: 'Wendy',
-		apellido: 'Vila',
-		email: 'vitae.risus@outlook.couk',
-		createAt: '2022-04-05'
-	},
-	{
-		id: 14,
-		nombre: 'Richard',
-		apellido: 'Vila',
-		email: 'sagittis.duis@google.couk',
-		createAt: '2022-04-23'
-	},
-	{
-		id: 15,
-		nombre: 'Jaden',
-		apellido: 'Castro',
-		email: 'odio@yahoo.org',
-		createAt: '2022-04-30'
-	},
-	{
-		id: 16,
-		nombre: 'Indigo',
-		apellido: 'Lorenzo',
-		email: 'turpis.egestas@outlook.edu',
-		createAt: '2022-05-14'
-	},
-	{
-		id: 17,
-		nombre: 'Philip',
-		apellido: 'Diaz',
-		email: 'cras.pellentesque@yahoo.ca',
-		createAt: '2022-04-18'
-	},
-	{
-		id: 18,
-		nombre: 'Jackson',
-		apellido: 'Moreno',
-		email: 'mattis@yahoo.net',
-		createAt: '2022-04-21'
-	},
-	{
-		id: 19,
-		nombre: 'Pearl',
-		apellido: 'Redondo',
-		email: 'varius@icloud.net',
-		createAt: '2022-04-21'
-	},
-	{
-		id: 20,
-		nombre: 'Jared',
-		apellido: 'Sanchez',
-		email: 'eget.volutpat.ornare@protonmail.couk',
-		createAt: '2022-05-08'
-	}
-  ];
+  clientes: Cliente[];
   constructor() { }
 
   ngOnInit(): void {
+    this.clientes = CLIENTES;
   }
 
 }
