6321ad6ad8523b9efd0d88bae40b22aea8a0d2f0
KeyzerH <keyzerhernandez.kjnhns@gmail.com>
2022-04-28 16:46:01 -0600

    Implementación de Servicio Angular con HttpClient

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { ClientesComponent } from './clientes/clientes.component';
 import { ClienteService } from './clientes/cliente.service';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HttpClientModule } from '@angular/common/http';
+
 const routes: Routes = [
   {path: '', redirectTo: '/clientes', pathMatch: 'full'},
   {path: 'directivas', component: DirectivaComponent},
@@ -25,6 +27,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot(routes)
   ],
   providers: [ClienteService],
--- clientes-app/src/app/clientes/cliente.service.ts
+++ clientes-app/src/app/clientes/cliente.service.ts
@@ -4,14 +4,19 @@ import { CLIENTES } from './clientes.json';
 import { Observable } from 'rxjs';
 import { of } from 'rxjs';
 
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private urlEndpoint:string = 'http://localhost:8080/api/clientes';
+  constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+    // return of(CLIENTES);
+    return this.http.get<Cliente[]>(this.urlEndpoint);
   }
 }
