ff3a19182a40dd5eae59fb8587841a9b1d40a9e5
KeyzerH <keyzerhernandez.kjnhns@gmail.com>
2022-04-28 17:42:37 -0600

    Agregando campos NIT y Teléfono

--- clientes-app/src/app/clientes/clientes.json.ts
+++ clientes-app/src/app/clientes/clientes.json.ts
@@ -5,140 +5,160 @@ export const CLIENTES: Cliente[] = [
   id: 1,
   nombre: 'Maisie',
   apellido: 'Serrano',
-  email: 'aliquet.vel@yahoo.com',
+  nit: '12345',
+  telefono: '87654321',
   createAt: '2022-05-05'
 },
 {
   id: 2,
   nombre: 'Dana',
   apellido: 'Lozano',
-  email: 'quis.pede@aol.couk',
+  nit: '12345',
+  telefono: '87654321',
   createAt: '2022-04-20'
 },
 {
   id: 3,
   nombre: 'Kimberley',
   apellido: 'Delgado',
-  email: 'cubilia.curae.donec@outlook.net',
+  nit: '12345',
+  telefono: '87654321',
   createAt: '2022-04-02'
 },
 {
   id: 4,
   nombre: 'Ralph',
   apellido: 'Ramos',
-  email: 'hendrerit.neque@google.edu',
+  nit: '12345',
+  telefono: '87654321',
   createAt: '2022-04-30'
 },
 {
   id: 5,
   nombre: 'Hayden',
   apellido: 'Hernandez',
-  email: 'nulla.integer.urna@aol.net',
+  nit: '12345',
+  telefono: '87654321',
   createAt: '2022-05-16'
 },
 {
   id: 6,
   nombre: 'Micah',
   apellido: 'Cruz',
-  email: 'accumsan.interdum@yahoo.net',
+  nit: '12345',
+  telefono: '87654321',
   createAt: '2022-04-18'
 },
 {
   id: 7,
   nombre: 'Neil',
   apellido: 'Gomez',
-  email: 'eget.magna@icloud.couk',
+  nit: '12345',
+  telefono: '87654321',
   createAt: '2022-05-11'
 },
 {
   id: 8,
   nombre: 'Ali',
   apellido: 'Gutierrez',
-  email: 'a@hotmail.couk',
+  nit: '12345',
+  telefono: '87654321',
   createAt: '2022-05-24'
 },
 {
   id: 9,
   nombre: 'Slade',
   apellido: 'Mora',
-  email: 'dui.cum.sociis@hotmail.couk',
+  nit: '12345',
+  telefono: '87654321',
   createAt: '2022-04-13'
 },
 {
   id: 10,
   nombre: 'Geraldine',
   apellido: 'Gallego',
-  email: 'diam.sed.diam@protonmail.com',
+  nit: '12345',
+  telefono: '87654321',
   createAt: '2022-04-20'
 },
 {
   id: 11,
   nombre: 'Elton',
   apellido: 'Miguel',
-  email: 'ligula.elit@outlook.ca',
+  nit: '12345',
+  telefono: '87654321',
   createAt: '2022-05-19'
 },
 {
   id: 12,
   nombre: 'Amery',
   apellido: 'Campos',
-  email: 'orci@aol.couk',
+  nit: '12345',
+  telefono: '87654321',
   createAt: '2022-04-07'
 },
 {
   id: 13,
   nombre: 'Wendy',
   apellido: 'Vila',
-  email: 'vitae.risus@outlook.couk',
+  nit: '12345',
+  telefono: '87654321',
   createAt: '2022-04-05'
 },
 {
   id: 14,
   nombre: 'Richard',
   apellido: 'Vila',
-  email: 'sagittis.duis@google.couk',
+  nit: '12345',
+  telefono: '87654321',
   createAt: '2022-04-23'
 },
 {
   id: 15,
   nombre: 'Jaden',
   apellido: 'Castro',
-  email: 'odio@yahoo.org',
+  nit: '12345',
+  telefono: '87654321',
   createAt: '2022-04-30'
 },
 {
   id: 16,
   nombre: 'Indigo',
   apellido: 'Lorenzo',
-  email: 'turpis.egestas@outlook.edu',
+  nit: '12345',
+  telefono: '87654321',
   createAt: '2022-05-14'
 },
 {
   id: 17,
   nombre: 'Philip',
   apellido: 'Diaz',
-  email: 'cras.pellentesque@yahoo.ca',
+  nit: '12345',
+  telefono: '87654321',
   createAt: '2022-04-18'
 },
 {
   id: 18,
   nombre: 'Jackson',
   apellido: 'Moreno',
-  email: 'mattis@yahoo.net',
+  nit: '12345',
+  telefono: '87654321',
   createAt: '2022-04-21'
 },
 {
   id: 19,
   nombre: 'Pearl',
   apellido: 'Redondo',
-  email: 'varius@icloud.net',
+  nit: '12345',
+  telefono: '87654321',
   createAt: '2022-04-21'
 },
 {
   id: 20,
   nombre: 'Jared',
   apellido: 'Sanchez',
-  email: 'eget.volutpat.ornare@protonmail.couk',
+  nit: '12345',
+  telefono: '87654321',
   createAt: '2022-05-08'
 }
 ];
