85c36812bee7c68c7ea99754dafba9216ef1c84b
KeyzerH <keyzerhernandez.kjnhns@gmail.com>
2022-05-15 18:38:17 -0600

    Validación de la imagen antes de subirla

--- clientes-app/src/app/clientes/detalle/detalle.component.ts
+++ clientes-app/src/app/clientes/detalle/detalle.component.ts
@@ -32,14 +32,22 @@ export class DetalleComponent implements OnInit {
   seleccionarFoto(event){
     this.fotoSeleccionada = event.target.files[0];
     console.log(this.fotoSeleccionada);
+    if(this.fotoSeleccionada.type.indexOf('image') < 0){
+      swal.fire('Tipo de archivo no admitido: ', 'El archivo debe ser del tipo imagen', 'error');
+      this.fotoSeleccionada = null;
+    }
   }
 
   subirFoto(){
-    this.clienteService.subirFoto(this.fotoSeleccionada, this.cliente.id)
-    .subscribe(cliente => {
-      this.cliente = cliente;
-      swal.fire('La foto se ha subido completamente!', `La foto se ha subido con éxito: ${this.cliente.foto}`, 'success');
-    });
+    if (!this.fotoSeleccionada) {
+      swal.fire('Error: ', 'Debe seleccionar una foto', 'error');
+    } else {
+      this.clienteService.subirFoto(this.fotoSeleccionada, this.cliente.id)
+        .subscribe(cliente => {
+          this.cliente = cliente;
+          swal.fire('La foto se ha subido completamente!', `La foto se ha subido con éxito: ${this.cliente.foto}`, 'success');
+        });
+    }
   }
 
 }
